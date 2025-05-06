752f649e0c708f8b38df0ffc1584a7a1ecf64cbe
Raul Ignacio <ridgomez99@gmail.com>
2025-05-06 18:45:48 -0400

    anecdotes part 1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,86 +1,27 @@
 import { useState } from 'react'
 
 const App = () => {
-  const course = {
-    name: 'give feedback',
-    statistics: 'statistics'
-  }
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
 
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [counter, setCounter] = useState(0)
-  const increaseCounter = () => setCounter(counter + 1)
-
-  const increaseGood = () => {
-    setGood(good + 1)
-    increaseCounter(counter + 1)
+  const [selected, setSelected] = useState(0)
+  const showRandomAnecdote = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length)
+    setSelected(randomIndex)
   }
-  const increaseNeutral = () => {
-    setNeutral(neutral + 1)
-    increaseCounter(counter + 1)
-  }
-  const increaseBad = () => {
-    setBad(bad + 1)
-    increaseCounter(counter + 1)
-  }
-
   return (
-    <div>
-      <Header course={course.name} />
-      <Button onClick={increaseGood} text="good" />
-      <Button onClick={increaseNeutral} text="neutral" />
-      <Button onClick={increaseBad} text="bad" />
-      <Header course={course.statistics} />
-      <Statistics good={good} neutral={neutral} bad={bad} />
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <p>{anecdotes[selected]}</p>
+      <button onClick={showRandomAnecdote}>Show Random Anecdote</button>
     </div>
-  )
-}
-
-const Header = ({ course }) => {
-  return <h1>{course}</h1>
-}
-
-const Display = ({ text, counter }) => {
-  return (
-    <div>
-      {text}: {counter}
-    </div>
-  )
-}
-
-const Button = ({ onClick, text }) => {
-  return (
-    <button onClick={onClick}>
-      {text}
-    </button>
-  )
-}
-const StatisticLine = ({ text, value }) => {
-  return (
-    <tr>
-      <td>{text}</td>
-      <td>{value}</td>
-    </tr>
-  )
-}
-const Statistics = ({ good, neutral, bad }) => {
-  const total = good + neutral + bad
-  if (total === 0) {
-    return <p>No feedback given</p>
-  }
-
-  return (
-    <table>
-      <tbody>
-        <StatisticLine text="good" value={good} />
-        <StatisticLine text="neutral" value={neutral} />
-        <StatisticLine text="bad" value={bad} />
-        <StatisticLine text="all" value={total} />
-        <StatisticLine text="average" value={(good - bad) / total} />
-        <StatisticLine text="positive" value={(good / total) * 100 + ' %'} />
-      </tbody>
-    </table>
   )
 }
 
