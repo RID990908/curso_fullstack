99286659727a8d82bab2b73f7e7c2808398ab1e1
Raul Ignacio <ridgomez99@gmail.com>
2025-05-06 18:06:20 -0400

    unicafe part 2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -9,10 +9,21 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [counter, setCounter] = useState(0)
+  const increaseCounter = () => setCounter(counter + 1)
 
-  const increaseGood = () => setGood(good + 1)
-  const increaseNeutral = () => setNeutral(neutral + 1)
-  const increaseBad = () => setBad(bad + 1)
+  const increaseGood = () => {
+    setGood(good + 1)
+    increaseCounter(counter + 1)
+  }
+  const increaseNeutral = () => {
+    setNeutral(neutral + 1)
+    increaseCounter(counter + 1)
+  }
+  const increaseBad = () => {
+    setBad(bad + 1)
+    increaseCounter(counter + 1)
+  }
 
   return (
     <div>
@@ -24,6 +35,9 @@ const App = () => {
       <Display text="Good" counter={good} />
       <Display text="Neutral" counter={neutral} />
       <Display text="Bad" counter={bad} />
+      <Display text="All" counter={counter} />
+      <Display text="Average" counter={(good - bad) / counter} />
+      <Display text="Positive" counter={(good / counter) * 100 + ' %'} />
     </div>
   )
 }
