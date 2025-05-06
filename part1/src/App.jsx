3883be87ae694b5d2b382c3ab39d48290a12895f
Raul Ignacio <ridgomez99@gmail.com>
2025-05-06 18:00:05 -0400

    unicafe part 1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,30 +1,29 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
-  console.log('rendering with counter value', counter)
-
-  const increaseByOne = () => {
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
+  const course = {
+    name: 'give feedback',
+    statistics: 'statistics'
   }
 
-  const decreaseByOne = () => { 
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
-  }
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-  const setToZero = () => {
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
-  }
+  const increaseGood = () => setGood(good + 1)
+  const increaseNeutral = () => setNeutral(neutral + 1)
+  const increaseBad = () => setBad(bad + 1)
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={setToZero} text="zero" />
-      <Button onClick={decreaseByOne} text="minus" />
+      <Header course={course.name} />
+      <Button onClick={increaseGood} text="good" />
+      <Button onClick={increaseNeutral} text="neutral" />
+      <Button onClick={increaseBad} text="bad" />
+      <Header course={course.statistics} />
+      <Display text="Good" counter={good} />
+      <Display text="Neutral" counter={neutral} />
+      <Display text="Bad" counter={bad} />
     </div>
   )
 }
@@ -33,29 +32,15 @@ const Header = ({ course }) => {
   return <h1>{course}</h1>
 }
 
-const Content = ({ parts }) => {
+const Display = ({ text, counter }) => {
   return (
     <div>
-      {parts.map((part, index) => (
-        <Part key={index} name={part.name} exercises={part.exercises} />
-      ))}
+      {text}: {counter}
     </div>
   )
 }
 
-const Part = ({ name, exercises }) => {
-  return <p>{name}: {exercises}</p>
-}
-
-const Total = ({ total }) => {
-  return <p>Number of exercises {total}</p>
-}
-const Display = ({counter}) => {
-  return (
-    <div>{counter}</div>
-  )
-}
-const Button = ({onClick,text}) => {
+const Button = ({ onClick, text }) => {
   return (
     <button onClick={onClick}>
       {text}
