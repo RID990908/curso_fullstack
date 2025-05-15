1d7de701b7e5aefbac0c22bda041443658cb3146
Raul Ignacio <ridgomez99@gmail.com>
2025-05-15 17:14:17 -0400

    excersice 2.9

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '99999999'
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchTerm, setSearchTerm] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -18,13 +19,8 @@ const App = () => {
     }
 
     // Prevent adding duplicate names
-    if (persons.some(person => person.name === newName)) {
+    if (persons.some(person => person.name.toLowerCase() === newName.toLowerCase())) {
       alert(`${newName} is already added to phonebook`)
-      return
-    }
-
-    if (persons.some(person => person.number === newNumber)) {
-      alert(`${newNumber} is already added to phonebook`)
       return
     }
 
@@ -41,9 +37,23 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value)
+  }
+
+  const personsToShow = searchTerm
+    ? persons.filter(person =>
+      person.name.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+    : persons
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with: <input value={searchTerm} onChange={handleSearchChange} />
+      </div>
+      <h2>Add a new person</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -57,8 +67,8 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => (
-          <li key={person.name}>
+        {personsToShow.map(person => (
+          <li key={person.id}>
             {person.name} {person.number}
           </li>
         ))}
