ec39c3f00f8aea3519e5e6de53166c03cb669fe7
Raul Ignacio <ridgomez99@gmail.com>
2025-05-15 17:47:23 -0400

    excersice 2.11

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
@@ -13,6 +14,13 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchTerm, setSearchTerm] = useState('')
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -46,8 +54,8 @@ const App = () => {
 
   const personsToShow = searchTerm
     ? persons.filter(person =>
-        person.name.toLowerCase().includes(searchTerm.toLowerCase())
-      )
+      person.name.toLowerCase().includes(searchTerm.toLowerCase())
+    )
     : persons
 
   return (
